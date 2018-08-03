c1fb9f8bef5beb16a57ea57d525c97fb81c82677
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-03 13:53:29 +0200

    Modification de la TabBar

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,22 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by TOGNI Elie on 03/08/2018.
+//  Copyright © 2018 TOGNI Elie. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE   //Couleur de la TabBar
+        tabBar.tintColor = GRIS_TRES_CLAIR      //Couleur du text de la TabBar
+
+      
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,15 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by TOGNI Elie on 03/08/2018.
+//  Copyright © 2018 TOGNI Elie. All rights reserved.
+//
+
+import UIKit
+
+// Création de constantes réutilisables dans le projet
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
