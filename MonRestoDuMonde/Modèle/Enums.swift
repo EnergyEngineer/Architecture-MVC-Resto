1e8f145a3239f0486e72e63f985889cec02368c4
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-03 15:04:18 +0200

    Class Plats

---
+++ MonRestoDuMonde/Modèle/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by TOGNI Elie on 03/08/2018.
+//  Copyright © 2018 TOGNI Elie. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+        
+}
